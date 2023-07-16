1851b08f460da3521b49f98f382d5841bac222c4
Moi <moisesexpositoalonso@gmail.com>
2023-07-16 09:41:23 -0700

    how to install fortran in new Mac

---
+++ instalRpackage_fortranpath.sh
@@ -0,0 +1,2 @@
+Rscript -e "install.packages('igraph', repos='http://cran.r-project.org', type='source', configure.args='--with-F77=/opt/homebrew/bin/gfortran --with-F90=/opt/homebrew/bin/gfortran')"
+
